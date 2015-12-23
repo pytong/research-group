8b9965547dfeb6f1e826973714da45c579f6fa25
Puiyee Tong <pytong07@gmail.com>
2015-12-22 20:08:56 -0700

    Moved css to header and script to footer

--- wp-content/themes/twentyten-child/footer.php
+++ wp-content/themes/twentyten-child/footer.php
@@ -55,6 +55,22 @@
 
 	wp_footer();
 ?>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <script src="//static.getclicky.com/js" type="text/javascript"></script>
+        <script type="text/javascript">try{ clicky.init(100838680); }catch(e){}</script>
+
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-62085773-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+
 </body>
 </html>
--- wp-content/themes/twentyten-child/header.php
+++ wp-content/themes/twentyten-child/header.php
@@ -2,115 +2,88 @@
 /**
  * Header template for our theme
  *
- * Displays all of the <head> section and everything up till <div id="main">.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+* Displays all of the <head> section and everything up till <div id="main">.
+*
+* @package WordPress
+* @subpackage Twenty_Ten
+* @since Twenty Ten 1.0
+*/
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-        <meta name=viewport content="width=device-width, initial-scale=1">
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name=viewport content="width=device-width, initial-scale=1">
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
+/*
+ * Print the <title> tag based on what is being viewed.
+ */
+global $page, $paged;
 
-	wp_title( '|', true, 'right' );
+wp_title( '|', true, 'right' );
 
-	// Add the blog name.
-	bloginfo( 'name' );
+// Add the blog name.
+bloginfo( 'name' );
 
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+// Add the blog description for the home/front page.
+$site_description = get_bloginfo( 'description', 'display' );
+if ( $site_description && ( is_home() || is_front_page() ) )
+	echo " | $site_description";
 
-	// Add a page number if necessary:
-	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
+// Add a page number if necessary:
+if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() )
+	echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
 
-	?></title>
+?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
-	/*
-	 * We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
+/*
+ * We add some JavaScript to pages with the comment form
+ * to support sites with threaded comments (when in use).
+ */
+if ( is_singular() && get_option( 'thread_comments' ) )
+	wp_enqueue_script( 'comment-reply' );
 
-	/*
-	 * Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
+/*
+ * Always have wp_head() just before the closing </head>
+ * tag of your theme, or you will break many plugins, which
+ * generally use this hook to add elements to <head> such
+ * as styles, scripts, and meta tags.
+ */
+wp_head();
 ?>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-62085773-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
-<script type="text/javascript">
-var clicky_site_ids = clicky_site_ids || [];
-clicky_site_ids.push(100838680);
-(function() {
-  var s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.async = true;
-  s.src = '//static.getclicky.com/js';
-  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
-})();
-</script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100838680ns.gif" /></p></noscript>
-
-</head>
 
 <body <?php body_class(); ?>>
 <div id="wrapper" class="hfeed">
-	<div id="header">
-		<div id="masthead">
-			<div id="branding" role="banner">
-				<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
-				<<?php echo $heading_tag; ?> id="site-title">
-					<span>
-						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</span>
-				</<?php echo $heading_tag; ?>>
-				<img class="uottawa_logo" src="http://www.julianchangroup.com/wp-content/uploads/2015/01/UOttawa-Logo.jpg" />
-				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+<div id="header">
+	<div id="masthead">
+		<div id="branding" role="banner">
+			<?php $heading_tag = ( is_home() || is_front_page() ) ? 'h1' : 'div'; ?>
+			<<?php echo $heading_tag; ?> id="site-title">
+				<span>
+					<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				</span>
+			</<?php echo $heading_tag; ?>>
+			<img class="uottawa_logo" src="http://www.julianchangroup.com/wp-content/uploads/2015/01/UOttawa-Logo.jpg" />
+			<div id="site-description"><?php bloginfo( 'description' ); ?></div>
 
-				<?php
-					// Compatibility with versions of WordPress prior to 3.4.
-					if ( function_exists( 'get_custom_header' ) ) {
-						/*
-						 * We need to figure out what the minimum width should be for our featured image.
-						 * This result would be the suggested width if the theme were to implement flexible widths.
-						 */
-						$header_image_width = get_theme_support( 'custom-header', 'width' );
-					} else {
-						$header_image_width = HEADER_IMAGE_WIDTH;
-					}
+			<?php
+				// Compatibility with versions of WordPress prior to 3.4.
+				if ( function_exists( 'get_custom_header' ) ) {
+					/*
+					 * We need to figure out what the minimum width should be for our featured image.
+					 * This result would be the suggested width if the theme were to implement flexible widths.
+					 */
+					$header_image_width = get_theme_support( 'custom-header', 'width' );
+				} else {
+					$header_image_width = HEADER_IMAGE_WIDTH;
+				}
 
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() && current_theme_supports( 'post-thumbnails' ) &&
+				// Check if this is a post or page, if it has a thumbnail, and if it's a big one
+				if ( is_singular() && current_theme_supports( 'post-thumbnails' ) &&
 							has_post_thumbnail( $post->ID ) &&
 							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail' ) ) &&
 							$image[1] >= $header_image_width ) :
