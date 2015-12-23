aabb229457d62f2eb71e14704a1e3d439e4ae5a6
Puiyee Tong <pytong07@gmail.com>
2015-12-22 21:23:20 -0700

    git puy Revert "Added awesome"
    This reverts commit 439b23481a6ffe9ef73e9f8005d7bb356139a894.

--- wp-content/themes/twentyten/header.php
+++ wp-content/themes/twentyten/header.php
@@ -35,7 +35,6 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
